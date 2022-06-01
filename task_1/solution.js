db5b3c72fd4e577214983dd4749c3876058d680b
Helen Krysova <misch.elena@rambler.ru>
2022-06-01 22:55:28 +0300

    ошибки исправлены

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,9 +1,9 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-    let message;
-    message = productName 'за' productPrice 'теперь в корзине';
-
+    
+    let message = productName;
+    console.log(message);
     
     // Задание №1.1. Оповещение о добавлении в корзину
 
@@ -19,13 +19,16 @@ function addInCartMessage(title, price) {
 
     // Конец решения задания №1.1.
 
-    return message;
+    return `${message} за ${productPrice} теперь в корзине!`;
 }
 
 function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = oldValue;
+
+    newValue = oldValue + 1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -41,6 +44,8 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
+    let newSum = oldSum + difference;
+    let newSumText = `${newSum} ₽`;
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
